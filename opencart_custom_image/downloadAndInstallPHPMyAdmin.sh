6c79957ac4c40af0a601da2b79e059696fa4b261
Tadeo Rivera-Pineda <xtecuan@protonmail.com>
2020-10-13 02:32:27 -0600

    Arreglo permisos

--- opencart_custom_image/downloadAndInstallPHPMyAdmin.sh
+++ opencart_custom_image/downloadAndInstallPHPMyAdmin.sh
@@ -6,11 +6,11 @@ VERSION=phpMyAdmin-5.0.3-all-languages
 
 curl https://files.phpmyadmin.net/phpMyAdmin/5.0.3/${VERSION}.zip --output /tmp/phpmyadmin.zip
 mkdir -p /var/lib/phpmyadmin/tmp
-chown -R www-data:www-data /var/lib/phpmyadmin
+chown -R daemon:daemon /var/lib/phpmyadmin
 unzip /tmp/phpmyadmin.zip -d $INSTALL_DIR
 mv $INSTALL_DIR/$VERSION $INSTALL_DIR/phpmyadmin
 cp $INSTALL_DIR/phpmyadmin/config.sample.inc.php $INSTALL_DIR/phpmyadmin/config.inc.php
-chown -R www-data:www-data $INSTALL_DIR/phpmyadmin
+chown -R daemon:daemon $INSTALL_DIR/phpmyadmin
 
 
 
